6494a08323648c0aa58cc440dffa94dd5a8b2d9c
Romain Tribes <tribes.romain@gmail.com>
2011-10-09 12:49:57 +0200

    Cleaner home page.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    remember_me = params[:session][:remember_me] == '1' ? true : false
-    user        = User.find_by_email(params[:session][:email])
+    user = User.find_by_email(params[:session][:email])
     
     if debug? && user
       cookies[:persistence_token] = user.persistence_token
       redirect_to dashboard_path
     elsif user && user.authenticate(params[:session][:password])
-      if remember_me
-        cookies.permanent[:persistence_token] = user.persistence_token
-      else
-        cookies[:persistence_token] = user.persistence_token
-      end
-        redirect_to dashboard_path
+      cookies.permanent[:persistence_token] = user.persistence_token
+      redirect_to dashboard_path
     else
-      flash[:email]       = params[:session][:email]
-      flash[:remember_me] = remember_me
-      flash[:error]       = true
+      flash[:email] = params[:session][:email]
+      flash[:error] = true
       redirect_to root_path
     end
   end
