729aac9416a16299633d4ec1c89f2bf1fd4eb2f2
Romain Tribes <tribes.romain@gmail.com>
2011-10-21 12:39:14 +0200

    Refactor test for units dispatch.

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -1,13 +1,12 @@
 require 'spec_helper'
 
 describe Participation do
-  let(:participation) { FactoryGirl.create(:participation_with_territories) }
+  let(:participation)         { FactoryGirl.create(:participation_with_territories) }
+  let(:units_to_deploy_count) { participation.units_count }
+  let(:deployed_units_count)  { participation.ownerships.sum(:units_count) }
   
   describe "#dispatch_remaining_units!" do
-    before :each do
-      @units_to_deploy_count = participation.units_count
-      @deployed_units_count  = participation.ownerships.sum(:units_count)
-      
+    before do
       participation.dispatch_remaining_units!
     end
     
@@ -18,7 +17,7 @@ describe Participation do
     
     
     it "should have the right number of units deployed" do
-      expected_units_count = @units_to_deploy_count + @deployed_units_count
+      expected_units_count = units_to_deploy_count + deployed_units_count
       actual_units_count   = participation.ownerships.sum(:units_count)
       
       actual_units_count.should == expected_units_count
